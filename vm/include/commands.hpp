87c935d0286769229c8b76be354472c5beb02f20
slava15923 <texet.com@yandex.ru>
2024-12-09 17:37:40 +0300

    добавил basecommands

--- vm/include/commands.hpp
+++ vm/include/commands.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#define NUM_COMMANDS 1
+#define NUM_COMMANDS 2
+#define NUM_BASE_COMMANDS 0
 #define NUM_CONSOLE_COMMANDS 2
 
 #include <line.hpp>
@@ -9,9 +10,25 @@
 
 void (*command[NUM_COMMANDS])(void*);
 
+//BASE_COMMANDS
+
+void (*basecommands[NUM_BASE_COMMANDS])(void*);
+
 //CONSOLE_COMMANDS
 
 void (*consolecommands[NUM_CONSOLE_COMMANDS])(void*);
+
+namespace consoleCommand {
+    void mov(void* l);//0 копирование данных из одного адресса в другой(итог будет записан в первый указанный адресс)
+    void add(void* l);//1 добавление из одного адресса в другой(итог будет записан в первый указанный адресс)
+    void sub(void* l);//2 отнимание из одного адресса в другой(итог будет записан в первый указанный адресс)
+    void mul(void* l);//3 умножение из одного адресса в другой(итог будет записан в первый указанный адресс)
+    void imul(void* l);//4 деление без остатка(итог будет записан в первый указанный адресс)
+    void rimul(void* l);//5 деление с остатком(итог будет записан в первый указанный адресс)
+    void rmul(void* l);//6 деление только с остатком(итог будет записан в первый указанный адресс)
+    void malloc(void* l);//7 выделение памяти
+    void free(void* l);//8 очищение памяти
+}
 
 namespace consoleCommand {
     void print(void* l);//0
@@ -19,7 +36,8 @@ namespace consoleCommand {
 }
 
 namespace commands {
-    void console_Command(void* l);//0
+    void base_Command(void* l);
+    void console_Command(void* l);//1
 }
 
 uint init_commands();
